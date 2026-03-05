5b59ebeaa77ee560bf04fba6e859f1eb9f4eb048
Nodirbek Rasuljonov <nodirbekrasuljonov@Nodirbeks-MacBook-Air.local>
2026-03-05 16:24:30 +0100

    task 2 is done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    float weightRasuljonovEarth=60.0f;
-    float marsGravity=0.38f;
-    float weightRasuljonovMars;
-    double weightRasuljonovMarsDouble;
-    int weightRasuljonovMarsInt;
-    char weightRasuljonovMarsChar;
-    int charwithOperation;
+
     //Task1
     /*public void main(String[] args){
+        float weightRasuljonovEarth=60.0f;
+        float marsGravity=0.38f;
+        float weightRasuljonovMars;
+        double weightRasuljonovMarsDouble;
+        int weightRasuljonovMarsInt;
+        char weightRasuljonovMarsChar;
+        int charwithOperation;
+
         //getting weight on Mars
         weightRasuljonovMars=weightRasuljonovEarth*marsGravity;
         System.out.println(weightRasuljonovMars);
@@ -31,6 +35,22 @@ public class Main {
         charwithOperation=weightRasuljonovMarsChar+5;
         System.out.println(charwithOperation);
     }*/
+
+    /*
+    //Task 2
+    public void main(String[] args){
+        //get subclass from Random class
+        Random random=new  Random();
+        //get random number
+        int randomNum=random.nextInt(100);
+        if (randomNum%2==0){
+            System.out.println("Even: "+randomNum);
+        }else {
+            System.out.println("Odd:  "+randomNum);
+        }
+
+    }*/
+
 
 
 }
