e122ede128593d1493b26f671a554ba1947fb683
Nodirbek Rasuljonov <nodirbekrasuljonov@Nodirbeks-MacBook-Air.local>
2026-03-05 10:14:36 +0100

    added first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    double price=9.99;
+    int amount=10000;
+    double vat=23;
+    double sum;
+
+}
