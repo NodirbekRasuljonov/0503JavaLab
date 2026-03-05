3dd083fc74aa89941b1035ef5bacf27909d5b861
Nodirbek Rasuljonov <nodirbekrasuljonov@Nodirbeks-MacBook-Air.local>
2026-03-05 16:10:51 +0100

    task 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    double price=9.99;
-    int amount=10000;
-    double vat=23;
-    double sum;
+public class Main {
+    float weightRasuljonovEarth=60.0f;
+    float marsGravity=0.38f;
+    float weightRasuljonovMars;
+    double weightRasuljonovMarsDouble;
+    int weightRasuljonovMarsInt;
+    char weightRasuljonovMarsChar;
+    int charwithOperation;
+    //Task1
+    /*public void main(String[] args){
+        //getting weight on Mars
+        weightRasuljonovMars=weightRasuljonovEarth*marsGravity;
+        System.out.println(weightRasuljonovMars);
+
+        //converting float weight to double
+        weightRasuljonovMarsDouble=weightRasuljonovMars;
+        System.out.printf("%.4f\n",weightRasuljonovMarsDouble);
+
+        //converting double weight to int
+        weightRasuljonovMarsInt= (int)weightRasuljonovMarsDouble;
+        System.out.println(weightRasuljonovMarsInt);
+
+        //convert int to char
+        weightRasuljonovMarsChar=(char) weightRasuljonovMarsInt;
+        System.out.println(weightRasuljonovMarsChar);
+
+
+        //operation with char
+        charwithOperation=weightRasuljonovMarsChar+5;
+        System.out.println(charwithOperation);
+    }*/
+
 
 }
